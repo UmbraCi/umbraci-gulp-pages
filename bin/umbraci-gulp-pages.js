70cfd7fb7fcfa745e4ac05281526f4b3241ab8cd
UmbraCi <umbraci@qq.com>
2022-08-03 10:49:57 +0800

    update gulp-sass to @5

--- bin/umbraci-gulp-pages.js
+++ bin/umbraci-gulp-pages.js
@@ -2,7 +2,7 @@
 
 // 把gulp命令行执行的构建任务指向lib/index.js里
 process.argv.push('--cwd')
-process.argv.push(process.cwd())  // 指定项目工作目录为当前命令行所在的目录，一般都是再项目根目录下
+process.argv.push(process.cwd()) // 指定项目工作目录为当前命令行所在的目录，一般都是再项目根目录下
 process.argv.push('--gulpfile')
 process.argv.push(require.resolve('..'))
 
